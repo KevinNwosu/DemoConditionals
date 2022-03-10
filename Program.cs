bceda489e69371ef5bc3772593e42fa2e56913ce
Kevin Nwosu <KKNwosu@dev-10.com>
2022-03-10 11:05:58 -0600

    Range checking code with proper range values

--- Program.cs
+++ Program.cs
@@ -13,10 +13,13 @@
         Console.Write("Enter a range number: ");
         range = int.Parse(Console.ReadLine());
 
-        Console.WriteLine($"Given a starting number of {startNum} and a target number of {targNum}. It is {IsWithinRange(startNum, targNum, range)}.");
+        string results = IsWithinRange(startNum, targNum, range) ? "within range" : "not within range";
+
+        Console.WriteLine($"Given a starting number of {startNum} and a target number of {targNum}. It is {results}.");
     }
     private static bool IsWithinRange(int num, int target, int range)
     {
-        return (num - target) <= range;
+        return ((num - target) <= range) && ((target - num) >= range);
     }
+    
 }
