862c1c0ebaf015f00917765721f6add8e65f157d
Kevin Nwosu <KKNwosu@dev-10.com>
2022-03-10 13:27:23 -0600

    New printing code with two prompt methods

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@
         Console.Write("Do we have paper? (y/n)");
         bool hasPaper = Console.ReadLine().ToLower() == "y";
 
-        Console.Write("What's the ink level in numbers?");
-        int inkLevel = int.Parse(Console.ReadLine());   
+
+        int inkLevel = PromptUserForNum("What is the ink level?");   
 
         PrintDoc(hasPower, hasPaper, inkLevel);
     }
@@ -24,4 +24,29 @@
         Console.Write(result);
     }
     
+    private static string PromptUser(string message)
+    {
+        Console.Write(message);
+        return Console.ReadLine();
+    }
+    private static int PromptUserForNum(string message)
+    {
+        bool isValid = false;
+        int numb;
+        do
+        {
+           
+            string input = PromptUser(message);
+
+            if (int.TryParse(input, out numb))
+            {
+                isValid = true;
+            }
+            else
+            {
+                Console.WriteLine("That is not a valid number, try again!");
+            }
+        } while (!isValid);
+        return numb;
+    }
 }
